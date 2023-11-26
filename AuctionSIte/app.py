ae2f21697ca9c1baee39417ff1c1585bfe46993a
Junchen Liang <liangjunchen88@gmail.com>
2023-11-26 12:39:53 -0600

    MicroService Merge and build front end for base dev

---
+++ AuctionSIte/app.py
@@ -0,0 +1,215 @@
+from flask import Flask, render_template, request, redirect, g, url_for, flash
+import os
+import database.db_connector as db
+from datetime import date
+from werkzeug.utils import secure_filename
+import auth
+from validation import validate_new_listing, validate_photo, validate_bid
+
+UPLOAD_FOLDER = 'static/img/'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# TODO: change to random bytes when deploying!
+app.config.from_mapping(SECRET_KEY='dev')
+app.register_blueprint(auth.bp)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    db_conn = db.connect_to_database()
+
+    query = "SELECT featureID, carFeature FROM Features;"
+    features = db.execute_query(
+        db_connection=db_conn, query=query).fetchall()
+
+    query = "SELECT listingID, featureID FROM FeaturesListings;"
+    listings_features = db.execute_query(
+        db_connection=db_conn, query=query).fetchall()
+
+    query = "SELECT bidID, bidAmt FROM Bids;"
+    bids = db.execute_query(db_connection=db_conn, query=query).fetchall()
+
+    query = "SELECT listingID, photoPath FROM Photos;"
+    photos = db.execute_query(
+        db_connection=db_conn, query=query).fetchall()
+
+    if request.method == 'GET':
+        query = "SELECT * FROM Listings WHERE userID IS NOT NULL AND expirationDate >= NOW();"
+        listings = db.execute_query(
+            db_connection=db_conn, query=query).fetchall()
+
+    elif request.method == 'POST':
+        search_query = f"%{request.form['searchquery']}%"
+        query = "SELECT * FROM Listings WHERE listingID IN (SELECT listingID FROM (SELECT L.listingID, CONCAT_WS(' ', L.year, L.make, L.model, F.carFeature) AS carInfo FROM Listings L INNER JOIN FeaturesListings FL ON L.listingID = FL.listingID INNER JOIN Features F ON FL.featureID = F.featureID) AS tmp WHERE tmp.carInfo LIKE %s) AND userID IS NOT NULL AND expirationDate >= NOW();"
+        listings = db.execute_query(db_connection=db_conn, query=query,
+                                    query_params=(search_query,)).fetchall()
+
+    return render_template('main.j2', listings=listings, listings_features=listings_features, features=features, bids=bids, photos=photos)
+
+
+@app.route('/place-bid/<int:list_id>', methods=['GET', 'POST'])
+def place_bid(list_id):
+    if request.method == 'POST':
+        bid_amt = int(request.form['bid'])
+        bid_date = date.today()
+        db_conn = db.connect_to_database()
+
+        # check if higher than current bid on listing
+        query = "SELECT l.listingID, l.bidID, b.bidAmt as amount FROM Listings l INNER JOIN Bids b ON l.bidID = b.bidID WHERE l.listingID = %s;"
+        high_bid = db.execute_query(db_connection=db_conn, query=query,
+                                    query_params=(list_id,)).fetchone()
+
+        valid_bid, message = validate_bid(bid_amt, high_bid)
+        if not valid_bid:
+            flash(message, 'danger')
+            return redirect(url_for('root'))
+        else:
+            query = "INSERT INTO Bids (userID, listingID, bidAmt, bidDate) VALUES (%s, %s, %s, %s)"
+            cursor = db.execute_query(db_connection=db_conn, query=query,
+                                      query_params=(g.user['userID'], list_id,
+                                                    bid_amt, bid_date))
+            bid_id = cursor.lastrowid
+
+            query = "UPDATE Listings SET bidID = %s WHERE listingID = %s;"
+            db.execute_query(db_connection=db_conn, query=query,
+                            query_params=(bid_id, list_id))
+
+            flash(message, 'success')
+            return redirect(url_for('root'))
+
+
+@app.route('/submit-listing', methods=['GET', 'POST'])
+@auth.login_required
+def submit_listing():
+    # features, years and car makes needed in both GET and POST requests
+    db_conn = db.connect_to_database()
+    query = "SELECT carFeature FROM Features;"
+    features = db.execute_query(db_connection=db_conn, query=query).fetchall()
+    makes = []
+    years = [_ for _ in range(date.today().year + 1, 1894, -1)]
+    
+    with open('./static/misc/car_manufacturers.txt', 'r') as manufacturers:
+        make = manufacturers.readline().rstrip("\n")
+        while make != '':
+            makes.append(make)
+            make = manufacturers.readline().rstrip("\n")
+
+    if request.method == "GET":
+        return render_template('submit_listing.j2', features=features, makes=makes, years=years)
+
+    if request.method == 'POST':
+        data = request.form
+        error = validate_new_listing(data)
+
+        if error:
+            flash(error, 'danger')
+            return render_template('submit_listing.j2', features=features, makes=makes, years=years)
+
+        # validated, parse form and add listing
+        make = data['make']
+        model = data['model']
+        year = int(data['year'])
+        mileage = int(data['mileage'])
+        reserve = int(data['reserve']) if data['reserve'] != '' else 0
+        list_date = date.today()
+        expiration = data['expiration']
+
+        # user photo stored at static/img/ otherwise default photo used
+        photo = request.files['photo']
+        filepath = "./static/img/No_image_available.jpg"
+        if validate_photo(photo): 
+            filename = secure_filename(photo.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            photo.save(filepath)
+
+        query = "INSERT INTO Listings (userID, make, model, year, mileage, reserve, listDate, expirationDate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+        cursor = db.execute_query(
+            db_connection=db_conn, query=query, query_params=(g.user['userID'], make, model, year, mileage, reserve, list_date, expiration))
+        list_id = cursor.lastrowid
+
+        query = "INSERT INTO Photos (photoPath, listingID) VALUES (%s, %s);"
+        db.execute_query(db_connection=db_conn, query=query,
+                         query_params=(filepath, list_id))
+
+        # handling of features input
+        sel_features = request.form.getlist('features')  # selected features
+        usr_feature = request.form['usrfeature']  # feature inputted by user
+
+        # determine whether user creating already existing feature
+        if len(usr_feature) != 0:
+            query = "SELECT * FROM Features WHERE carFeature=%s"
+            feature_dups = db.execute_query(
+                db_connection=db_conn,
+                query=query,
+                query_params=(usr_feature,)).fetchall()
+
+            # no duplicate, add new feature
+            if len(feature_dups) == 0:
+                query = "INSERT INTO Features (carFeature) VALUES (%s);"
+                db.execute_query(
+                    db_connection=db_conn,
+                    query=query,
+                    query_params=(usr_feature,))
+                sel_features.append(usr_feature)
+            # duplicate, associate existing feature with new listing
+            else:
+                sel_features.append(feature_dups[0]['carFeature']) 
+
+        # add all features associated with listing into featuresListings
+        if len(sel_features) > 0:
+            format_str = ','.join(['%s'] * len(sel_features))
+            query = "SELECT featureID FROM Features WHERE carFeature IN (" + \
+                format_str + ");"
+            feature_ids = db.execute_query(db_connection=db_conn, query=query, query_params=tuple(sel_features)).fetchall()
+
+            # insert all feature ids for this listing
+            listing_features = [(list_id, feature_id['featureID'])
+                                for feature_id in feature_ids]
+            query = "INSERT INTO FeaturesListings (listingID, featureID) VALUES (%s, %s);"
+            db.execute_many(
+                db_connection=db_conn, query=query, query_params=listing_features)
+
+        return redirect(url_for('root'))
+
+
+@ app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    db_conn = db.connect_to_database()
+
+    # requesting user profile stats
+    if request.method == "GET":
+        # gather user's active listings
+        user_id = (g.user['userID'], )
+        query = \
+            "SELECT l.listingID, l.year, l.make, l.model, b.bidAmt, l.reserve, l.expirationDate FROM Listings l\
+            LEFT JOIN Bids b ON l.bidID = b.bidID \
+            WHERE l.userID = %s;"
+        active_listings = db.execute_query(db_conn, query, user_id)
+
+        # gather user's bid history
+        query = \
+            "SELECT b.bidDate, l.year, l.make, l.model, b.bidAmt FROM Bids b \
+            INNER JOIN Listings l ON b.listingID = l.listingID \
+            WHERE b.userID = %s;"
+        bid_history = db.execute_query(db_conn, query, user_id)
+
+        return render_template('profile.j2', active_listings=active_listings, bid_history=bid_history)
+
+    # user requesting listing deletion
+    elif request.method == "POST":
+        # gather relevant data to delete listing
+        listing_to_delete = request.form['listingID']
+        delete_query = 'DELETE FROM FeaturesListings WHERE listingID = %s;'
+        update_query = 'UPDATE Listings SET Listings.userID = NULL WHERE listingID = %s;'
+
+        db.execute_query(db_conn, delete_query, (listing_to_delete,))
+        db.execute_query(db_conn, update_query, (listing_to_delete,))
+
+        return redirect(url_for('profile'))
+
+
+# Listener
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112))
+    app.run(port=port, debug=True)
---
+++ AuctionSIte/database/javaSavesUs_db.sql
@@ -0,0 +1,111 @@
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+    userID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(255) UNIQUE NOT NULL ,
+    password VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    dateJoined DATE NOT NULL,
+    PRIMARY KEY (userID)
+);
+
+INSERT INTO Users (userName, password, firstName, lastName, email, dateJoined) VALUES 
+    ('johndoe', 'pbkdf2:sha256:150000$iD5kR8qS$01a43a001a115b0747ed312a66686405225c1658ab8bf57f5a46e94d0393039e', 'John', 'Doe', 'johndoe@gmail.com', '2020-04-04'),
+    ('janedoe', 'pbkdf2:sha256:150000$jrbRgQyP$ff64293bbc3e65a67a48a9cb878d38327c5a17f6994b4e55205b5fbd67819257', 'Jane', 'Doe', 'janedoe@gmail.com', '2019-04-04'),
+    ('jimdoe', 'pbkdf2:sha256:150000$OlA1Whpp$53a114e19713849f5db98730c6fe70d0bf0b5afa2669a08ed5170f9b9ea95ca7', 'Jim', 'Doe', 'jimdoe@gmail.com', '2018-04-04');
+
+DROP TABLE IF EXISTS Listings;
+
+CREATE TABLE Listings (
+    listingID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    userID INT(11),
+    bidID INT(11),
+    make VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year INT(11) NOT NULL,
+    mileage INT(11) NOT NULL,
+    reserve INT(11) NOT NULL,
+    listDate DATE NOT NULL,
+    expirationDate DATE NOT NULL,
+    PRIMARY KEY (listingID),
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+INSERT INTO Listings (userID, bidID, make, model, year, mileage, reserve, listDate, expirationDate) VALUES 
+    (1, NULL, 'Ford', 'Mustang', 1965, 130000, 100000, '2020-04-04', '2022-06-06'),
+    (2, NULL, 'Porsche', 'Carrera', 1987, 150000, 110000, '2020-03-04', '2022-05-06'),
+    (3, NULL, 'Jeep', 'Wrangler', 2002, 10000, 0, '2020-02-04', '2022-04-06'),
+    (3, NULL, 'Chevrolet', 'Corvette Stingray', 1969, 45783, 1500000, '2020-01-04', '2022-02-03');
+
+DROP TABLE IF EXISTS Bids;
+
+CREATE TABLE Bids (
+    bidID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    userID INT(11) NOT NULL,
+    listingID INT(11) NOT NULL,
+    bidAmt INT(11) NOT NULL,
+    bidDate DATE NOT NULL,
+    PRIMARY KEY (bidID),
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID)
+);
+
+ALTER TABLE Listings
+ADD FOREIGN KEY (bidID) REFERENCES Bids(bidID);
+
+INSERT INTO Bids (userID, listingID, bidAmt, bidDate) VALUES 
+    (1, 1, 20000, '2020-04-05'),
+    (1, 2, 50000, '2020-03-06'),
+    (2, 3, 70000, '2020-02-08'),
+    (3, 1, 25000, '2020-04-06');
+
+UPDATE Listings SET bidID = 4 WHERE listingID = 1;
+UPDATE Listings SET bidID = 2 WHERE listingID = 2;
+UPDATE Listings SET bidID = 3 WHERE listingID = 3;
+
+DROP TABLE IF EXISTS Features;
+
+CREATE TABLE Features (
+    featureID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    carFeature VARCHAR(255) UNIQUE NOT NULL,
+    PRIMARY KEY (featureID)
+);
+
+INSERT INTO Features (carFeature) VALUES 
+    ('Manual'), ('Electric'), ('Convertible'), ('4WD'), ('Sunroof'), 
+    ('Automatic'), ('Leather Seats'), ('Air Conditioning'), ('Heated Seats'),
+    ('Automated Parking'), ('Bluetooth Technology'), ('Full Roof Rack');
+
+DROP TABLE IF EXISTS Photos;
+
+CREATE TABLE Photos (
+    photoID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    listingID INT(11) NOT NULL,
+    photoPath VARCHAR(255) NOT NULL,
+    PRIMARY KEY (photoID),
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID)
+);
+
+INSERT INTO Photos (listingID, photoPath) VALUES
+    (1, 'static/img/65-mustang.jpg'),
+    (2, 'static/img/87-carrera.jpg'),
+    (3, 'static/img/02-wrangler.jpg'),
+    (4, 'static/img/69-stingray.jpg');
+
+DROP TABLE IF EXISTS FeaturesListings;
+
+CREATE TABLE FeaturesListings (
+    listingID INT(11),
+    featureID INT(11),
+    PRIMARY KEY (listingID, featureID),
+    FOREIGN KEY (listingID) REFERENCES Listings(listingID),
+    FOREIGN KEY (featureID) REFERENCES Features(featureID)
+);
+
+INSERT INTO FeaturesListings (listingID, featureID) VALUES
+    (1, 1), (1, 7), (1, 5),
+    (2, 5), (2, 6), (2, 8),
+    (3, 1), (3, 4), (3, 10),
+    (4, 1), (4, 3), (4, 7);
